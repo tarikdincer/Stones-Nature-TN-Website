214a6dd85a06ee2ecf0d455a02fb4374e91166ee
tarikdincer <51008424+tarikdincer@users.noreply.github.com>
2022-06-24 04:43:29 +0300

    ProductController added

---
+++ StonesNatureTnWeb/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StonesNatureTnWeb.Data;
+using StonesNatureTnWeb.Models;
+
+namespace StonesNatureTnWeb.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index(int productId)
+        {
+            ViewData["Product"] = _db.Categories.Find(productId);
+            ViewData["ProductImages"] = _db.ProductImages.Where(p => p.ProductId == productId).ToList();
+            ViewData["ProductDetails"] = _db.ProductDetail.Where(p => p.ProductId == productId).ToList();
+            ViewData["ProductSizes"] = _db.ProductSize.Where(p => p.ProductId == productId).ToList();
+            return View();
+        }
+    }
+}
